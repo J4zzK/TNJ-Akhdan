58f5a36cf9e3f4ebd4030dcf47bb0bf1e4447b92
J4zzK <akhdanjazarikautsar@gmail.com>
2024-06-01 20:18:52 +0700

    Create Pertemuan1.py
    Mencoba python

---
+++ Pertemuan1.py
@@ -0,0 +1,18 @@
+#belajar python
+print("hello Python")
+print(13)
+
+nama = "akahadan" #kata atau kalimat harus pakai petik
+print(nama)
+hobi = "main game"
+umur = "secukupnya" #tipe data integer atau nagka
+laki = "true" #tipe data bolean, true atau False
+
+# 6angkapertama = 1 #nama varriabel tidak boleh diawali dengan angka 
+nama_lengkap = "akahadan Jazarik" # snake_case
+nama = "akahadan"
+print(nama, umur)
+print(nama + umur)
+print(nama + hobi)
+print("nama saya adalah", nama)
+print("nama saya adalah", nama, "umur saya adalah", umur)
