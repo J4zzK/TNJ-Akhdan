ec463115797abbbc139b88a646b9a6b7f25e5411
J4zzK <akhdanjazarikautsar@gmail.com>
2024-06-08 10:58:25 +0700

    Pertemuan2.py

---
+++ Pertemuan2.py
@@ -0,0 +1,30 @@
+print("Robot siap beraksi") 
+print("Robot bergerak maju") #menampilkan teks "Robot bergerak maju"
+speed = ("kecepatan Robot = 10")
+print(speed)
+
+arah = "kiri"
+kecepatan = "5"
+durasi = "10 detik"
+print("Robot bergerak ke", arah, "dengan kecepatan", kecepatan, "Selama", durasi)
+
+
+status1 = "Robot aktif" 
+status2 = "menunggu perintah"
+print(status1, "dan", status2)
+
+print("Robot siap beraksi") 
+print("Robot bergerak maju") #menampilkan teks "Robot bergerak maju"
+speed = ("kecepatan Robot = 10")
+print(speed)
+
+arah = "kiri"
+kecepatan = "5"
+durasi = "10 detik"
+print("Robot bergerak ke", arah, "dengan kecepatan", kecepatan, "Selama", durasi)
+
+
+status1 = "Robot aktif" 
+status2 = "menunggu perintah"
+print(status1, "dan", status2)
+
