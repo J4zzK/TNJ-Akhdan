9019e23f662d14caa15531e3bb97bee42e19c4c8
J4zzK <akhdanjazarikautsar@gmail.com>
2024-06-08 20:27:23 +0700

    pertemuan3.py

---
+++ pertemuan3.py
@@ -0,0 +1,23 @@
+inventaris = ["printer", "Laptop", "Scanner", "Monitor"]
+print(inventaris)
+
+item_baru = "Keyboard"
+inventaris.append(item_baru)
+print(inventaris)
+
+karyawan = ["Andi", "Budi", "Citra", "Dewi"]
+print(karyawan)
+
+karyawan_baru = "Eka"
+karyawan.append(karyawan_baru)
+print(karyawan)
+
+harga_produk = [50000, 150000, 25000, 75000]
+print(harga_produk)
+
+harga_baru = 1000000
+harga_produk.append(harga_baru)
+print(harga_produk)
+
+print(len(inventaris))
+print(len(karyawan))
